1de97a69993579c20b12203be8528fad80df8120
Malik-Oman <malikoman258@gmail.com>
2026-04-05 08:35:47 -0700

    feat: setup homepage with hero section, authentication pages, and header component

---
+++ frontend/src/components/LoginModel.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { AnimatePresence, motion } from "motion/react";
+
+
+const LoginModel = ({ open, onClose }) => {
+  return (
+    <AnimatePresence>
+      {open && (
+        <motion.div
+          onClick={onClose}
+          className="fixed inset-0 z-[100] flex items-center justify-center bg-black/80 backdrop-blur-xl px-4"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <motion.div
+            initial={{ scale: 0.88, opacity: 0, y: 60 }}
+            animate={{ scale: 1, opacity: 1, y: 0 }}
+            exit={{ scale: 0.9, opacity: 0, y: 40 }}
+            transition={{ duration: 0.45, ease: "easeOut" }}
+            className="relative w-full max-w-md p-[1px] rounded-3xl bg-gradient-to-br from-purple-500/40 via-blue-500/30 to-transparent"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className="relative rounded-3xl bg-[#0b0b0b] border bordr-white/10 shadow-[0_30px_120px_rgba(0,0,0,08)] overflow-hidden">
+              <motion.div
+                animate={{ opacity: [0.25, 0.4, 0.25] }}
+                transition={{ duration: 6, repeat: Infinity }}
+                className="absolute -top-32 -left-32 w-80 h-80 bg-purple-500/30 blur-[140px] pointer-events-none"
+              />
+
+              <motion.div
+                animate={{ opacity: [0.25, 0.4, 0.25] }}
+                transition={{ duration: 6, repeat: Infinity, delay: 2 }}
+                className="absolute -bottom32 -right-32 w-80 h-80 bg-blue-500/25 blur-[140px] pointer-events-none"
+              />
+
+              <button
+                onClick={onClose}
+                className="absolute top-5 right-5 text-zinc-400 hover:text-white transition text-lg cursor-pointer z-10"
+              >
+                X
+              </button>
+
+              <div className="relative px-8 pt-14 pb-10 text-center ">
+                <h1 className="inlne-block mb-6 px-4 py-1.5 rounded-full bg-white/5 border boder-white/10 text-xs text-zinc-300  ">
+                  AI-Powered website builder
+                </h1>
+                <h2 className="text-3xl font-semibold leading-tight mb-3 space-x-2">
+                  <span>Welcome to</span>
+                  <span className="bg-linear-to-r from-purple-400 to-blue-400 bg-clip-text text-transparent">OmanWEB.AI</span>
+                </h2>
+
+                <motion.button
+                whileHover={{scale: 1.04}}
+                whileTap={{scale: 0.96}}
+                className="group relative w-full h-13 rounded-xl bg-white text-black font-semibold shadow-xl overflow-hidden "
+                >
+                  <div className="absolute inset-0 bg-linear-to-r from-zinc-100 to-white opacity-0 group-hover:opacity-100 transition"/>
+                  <div className="relative flex items-center justify-center gap-3">
+                     <img src="http://www.svgrepo.com/show/303108/google-icon-logo.svg" alt=""  className='h-6 w-10'  />
+                  Continue With Google
+                  </div>
+                 
+                </motion.button>
+
+                <div className="flex items-center gap-4 my-10">
+                  <div className="h-px flex-1 bg-white/10"/>
+                  <span className="text-xs text-zinc-500 tracking-wide">Secure Login</span>
+                  <div className="h-px flex-1 bg-white/10"/>
+                </div>
+
+                <p className="text-xs text-zinc-500 leading-relaxed">
+                  By Continuing, you agree to our{" "}
+                  <span className="underline cursor-pointer hover:text-zinc-300">
+                    Terms of services
+                  </span>
+                  and{" "}
+                  <span className="underline cursor-pointer hover:text-zinc-300">
+                    Privacy Policy.
+                  </span>
+                </p>
+
+              </div>
+
+            </div>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default LoginModel;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "motion/react";
+import LoginModel from "../components/LoginModel";
 
 const Home = () => {
+
+  const [openLogin, setOpenLogin] = useState(false)
+
+
+ const highlights = [
+      "AI Generated Code",
+      "Fully Responsive Layouts",
+      "Production Ready Output",
+ ]
+
   return (
     <div className="relative min-h-screen bg-[#040404] text-white overflow-hidden">
       <motion.div
@@ -18,7 +29,7 @@ const Home = () => {
             <div className="hidden md:inline text-sm text-zinc-400 hover:text-white cursor-pointer">
               Pricing
             </div>
-            <button className="px-4 py-2 rounded-lg border border-white/20 hover:bg-white/10 text-sm cursor-pointer">
+            <button onClick={()=>setOpenLogin(true)} className="px-4 py-2 rounded-lg border border-white/20 hover:bg-white/10 text-sm cursor-pointer">
               Get Started
             </button>
           </div>
@@ -41,7 +52,42 @@ const Home = () => {
             With AI
           </span>
         </motion.h1>
+
+        <motion.p
+           initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          className="mt-8 max-w-2xl mx-auto text-zinc-400 text-lg"
+        >
+          Describe your idea let AI generate a modern,
+          responsive, production-ready websites.
+        </motion.p>
+        
+        <button onClick={()=>setOpenLogin(true)} className=" px-10 py-4 rounded-xl bg-white text-black font-semibold hover:scale-105 transition mt-12 cursor-pointer">Get Started</button>
       </section>
+
+      <section className="max-w-7xl mx-auto px-6 pb-32">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
+          {highlights.map((h,i)=>(
+            <motion.div key={i} 
+            initial={{opacity:0, y:40}}
+            whileInView={{opacity:1, y:0}}
+            className="rounded-2xl bg-white/5 border border-white/10 p-8"
+            >
+                <h1 className="text-xl font-semibold mb-3">{h}</h1>
+                <p className="text-sm text-zinc-400">
+                  OmanWEB.AI builds real Websites = clean code,
+                  animation, responsiveness and scalable structure.
+                </p>
+            </motion.div>
+          ))}
+        </div>
+      </section>
+
+      <footer className="border-t border-white/10 py-10 text-center text-sm text-zinc-500">
+        &copy; {new Date().getFullYear()} OmanWEB.AI
+      </footer>
+
+    {openLogin && <LoginModel open={openLogin} onClose={() =>setOpenLogin(false)}/>}
     </div>
   );
 };
